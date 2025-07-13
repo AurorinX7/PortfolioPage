4371b040a1c8a3d4185c6698958cc2c1d68080e6
anuraag <priaynshu76@gmail.com>
2025-07-13 19:59:09 +0530

    idk

--- calisthenics page/script2.js
+++ calisthenics page/script2.js
@@ -73,3 +73,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+const menuBtn = document.getElementById('menu-btn');
+const sideMenu = document.getElementById('side-menu');
+
+menuBtn.onclick = function() {
+  sideMenu.classList.toggle('open');
+  menuBtn.classList.toggle('active');
+};
